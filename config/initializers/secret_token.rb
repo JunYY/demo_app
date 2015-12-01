9718402e4c5b1c2611777179c2df32a6b127c1d3
saasbook <saasbook@saasbook.(none)>
2015-12-01 04:37:43 -0400

    layout2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '076d2a384772e9b20d1726c6969f55b1272e80df59aca981f1e69793b247731d940ccd94a0bcd917523beb60581f2e9a7f9367a65ecf619f4328987994a6cf55'
+DemoApp::Application.config.secret_token = '9f080971dc3de95c274fa645270609ec73d201009c800ddcd685cc3a94492c0320d9374f2bd1997583851fe8ebcf80d3c9eb18f19a06210655887fba791d3502'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151027062121) do
+ActiveRecord::Schema.define(:version => 20151027060537) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
